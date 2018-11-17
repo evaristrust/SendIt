5f5ea8893e310586b9a1c18b303282d29759bdde
Evarist <evaristenizey@gmail.com>
2018-11-17 02:53:09 +0200

    Specific parcel detail

--- API/src/api/controllers/parcels.controller.js
+++ API/src/api/controllers/parcels.controller.js
@@ -3,7 +3,19 @@ import parcel from '../models/parcel';
 export default {
 
 
+    parcelDetail(req, res, next){
+        /*
+        Show detail of a specific parcel. Pass id of the parcel as part of the URL
+        */
 
+    const parcel_id = req.params.parcel_id;
+
+    // find the parcel in parcel array whose id is equal to parcel_id
+
+    const parcel_detail = parcel.filter(p => p.id == parcel_id);
+
+    res.send(parcel_detail);
+    }
     
    
 
--- API/src/api/routes.js
+++ API/src/api/routes.js
@@ -2,5 +2,5 @@ const express = require('express');
 import ParcelsController from './controllers/parcels.controller';
 
 const router = express.Router();
-
+router.get('/v1/parcels/:parcel_id', ParcelsController.parcelDetail);
 export default router;
