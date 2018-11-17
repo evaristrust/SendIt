b1693f2ce7199d41780168d39db2d93d28f61ffc
Evarist <evaristenizey@gmail.com>
2018-11-17 03:04:56 +0200

    Cancel an order

--- API/src/api/controllers/parcels.controller.js
+++ API/src/api/controllers/parcels.controller.js
@@ -2,7 +2,45 @@ import parcel from '../models/parcel';
 
 export default {
 
+    cancelParcel(req, res, next){
+        /*
+        Cancel the parcel. Pass parcelid as parameter
 
+        Only incomplete parcels can be cancelled
+        */
+        const parcel_id = req.params.parcel_id;
+
+         // get the parcel
+
+         let order = parcel.filter(p => p.id == parcel_id);
+
+         let http_status = 200;
+
+         let msg = '';
+
+         // did we find the parcel?
+
+         if(order.length == 0){
+            http_status = 400;
+            msg ='The parcel was not found';
+         }
+         else{
+            // the parcel exists but can it be cancelled?
+            
+            if (order[0].status == 'complete'){
+                http_status = 400;
+                msg = 'The parcel cannot be cancelled because it is already complete';
+            }
+            else{
+                msg='The order has been cancelled successfully';
+                order[0]['status'] = 'cancelled';
+            }
+         }
+
+            res.status(http_status).send(msg);
+         
+
+ }
 
     
    
--- API/src/api/routes.js
+++ API/src/api/routes.js
@@ -2,5 +2,5 @@ const express = require('express');
 import ParcelsController from './controllers/parcels.controller';
 
 const router = express.Router();
-
+router.put('/v1/parcels/:parcel_id/cancel', ParcelsController.cancelParcel);
 export default router;
