787e6aeaf75f16320df83944ea2f61460649b4b3
Evarist <evaristenizey@gmail.com>
2018-11-17 02:48:26 +0200

    Create New Parcel

--- API/src/api/routes.js
+++ API/src/api/routes.js
@@ -2,5 +2,5 @@ const express = require('express');
 import ParcelsController from './controllers/parcels.controller';
 
 const router = express.Router();
-
+router.post('/v1/parcels/', ParcelsController.newParcel);
 export default router;
