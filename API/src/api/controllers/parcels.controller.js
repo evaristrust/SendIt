71dc7dd3ee9a8775852f43affc4ba4770111d240
Evarist <evaristenizey@gmail.com>
2018-11-17 02:59:58 +0200

    Parcels of a specific User

--- API/src/api/controllers/parcels.controller.js
+++ API/src/api/controllers/parcels.controller.js
@@ -3,6 +3,20 @@ import parcel from '../models/parcel';
 export default {
 
 
+ userParcels(req, res, next){
+        /*
+        Show parcels ordered by a specific user
+        */
+
+    const user_id = req.params.user_id;
+
+    // find the parcels in parcel array whose id is equal to customer_id
+
+    const parcels = parcel.filter(p => p.customer_id == user_id);
+
+    res.send(parcels);
+    }
+
 
     
    
--- API/src/api/routes.js
+++ API/src/api/routes.js
@@ -2,5 +2,5 @@ const express = require('express');
 import ParcelsController from './controllers/parcels.controller';
 
 const router = express.Router();
-
+router.get('/v1/users/:user_id/parcels', ParcelsController.userParcels);
 export default router;
