fcf0d48dc451fbcca6023cb5bbd84d7f08aa6307
Evarist <evaristenizey@gmail.com>
2018-11-17 07:37:45 +0200

    Solved the conflict in the routes for user parcels

--- API/src/api/routes.js
+++ API/src/api/routes.js
@@ -2,10 +2,7 @@ const express = require('express');
 import ParcelsController from './controllers/parcels.controller';
 
 const router = express.Router();
-<<<<<<< HEAD
 router.get('/v1/parcels', ParcelsController.showParcels);
 router.post('/v1/parcels/', ParcelsController.newParcel);
-=======
 router.get('/v1/users/:user_id/parcels', ParcelsController.userParcels);
->>>>>>> ft-user-parcels-endpoint-162028267
 export default router;
