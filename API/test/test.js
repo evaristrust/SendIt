54cabdfd43035b9ebfeba29444739b2f047b59e5
Evarist <evaristenizey@gmail.com>
2018-11-18 20:24:15 +0200

    Test get all parcels endpoint

--- API/test/test.js
+++ API/test/test.js
@@ -7,3 +7,22 @@ var chaiHttp = require("chai-http");
 var app = require('../src/app');
 
 chai.use(chaiHttp);
+
+
+//testing on getting all parcels
+describe('/GET parcels', () => {
+	it('It should get all parcels we have', (done) => {
+		chai.request(app)
+		.get('/api/v1/parcels/')
+		
+		.end((err, res) => {
+			res.should.have.status(200);
+			expect(res.body.length).to.equal(3);
+			res.body.should.be.a('array');
+			done();
+		});
+
+	});
+
+});
+ 
